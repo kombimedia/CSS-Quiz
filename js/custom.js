e7e18bb17d1b929b4a84db3b724fd56364ff1d5c
kombimedia <cy@kombimedia.nz>
2017-07-01 11:59:22 +1200

    Next and Prev buttons
    Adding styling to show when buttons are valid or invalid

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 // Event listeners
+var getScore = document.getElementById('calculate-score')
+var haveAnotherGo = document.getElementById('restart-quiz');
 document.getElementById('start-quiz').addEventListener("click", signupValidate);
-document.getElementById('calculate-score').addEventListener("click", quizValidate);
-// document.getElementById('retry').addEventListener("click", resetQuiz);
-var reStartQuiz = document.getElementById('resit');
-reStartQuiz.addEventListener("click", resetQuiz);
+getScore.addEventListener("click", quizValidate);
+haveAnotherGo.addEventListener("click", resetQuiz);
 
 // Global variables
 var name, email, answers, radioButton, questionNumber;
@@ -90,54 +90,66 @@ function quizValidate() {
       errorMessage.innerHTML = ("Uh oh.. You haven't answered all the questions!<br>You still need to answer question(s) - " + unAnswered);
       return false;
     }
-    // Once all questions are answered hide the quiz form and show congratulations message
+    // Once all questions are answered and 'Get Score' is clicked, hide the quiz form and show congratulations message
     quizForm.style.display = 'none';
-    reStartQuiz.style.display = 'block';
+    haveAnotherGo.style.display = 'block';
     errorMessage.style.color = '#1ABC9C';
     errorMessage.innerHTML = ("<h2>Good work " + name.split(" ")[0] + "! You have completed the CSS quiz.</h2><h3>Your score is " + totalScore + " out of " + questionAmount + "!</h3>");
 }
 
-// Uncheck all radios, remove 'Have Another Go!' button, remove congratulations message, show quiz at question 1
+// Uncheck all radios, remove 'Have Another Go!' button, remove congratulations message, show quiz at question 1, reset 'next' and 'prev' buttons
 function resetQuiz(){
   var questionOne =  document.querySelector('.q1');
   var questionTen =  document.querySelector('.q10');
+  var nextButton = document.getElementById('next');
+  var prevButton = document.getElementById('prev');
     for (var questionNumber = 1; questionNumber <= questionAmount; questionNumber++) {
       var ansChecked = false;
       var answers = document.getElementsByName("question-" + questionNumber);
 
       for (var radioButton = 0; radioButton < answers.length; radioButton++) {
           answers[radioButton].checked = false;
-          reStartQuiz.style.display = 'none';
+          haveAnotherGo.style.display = 'none';
           quizForm.style.display = 'block';
           questionOne.className = 'content-box question active q1';
           questionTen.className = 'content-box question q10';
+          getScore.className = 'hidden';
+          nextButton.className = 'btn-pag';
+          prevButton.className = 'btn-pag btn-pag-invalid';
           errorMessage.style.color = '#FF0000';
           errorMessage.innerHTML = "";
       }
     }
 }
 
-// Remove 'Have Another Go!' button, remove congratulations message, reset and show quiz
-// function resitQuiz(){
-//   var questionOne =  document.querySelector('.q1');
-//   var questionTen =  document.querySelector('.q10');
-//   reStartQuiz.style.display = 'none';
-//   quizForm.style.display = 'block';
-//   questionOne.style.display = 'block';
-//   questionTen.style.display = 'none';
-//   errorMessage.style.color = '#FF0000';
-//   errorMessage.innerHTML = "";
-//   resetQuiz();
-// }
-
-// Quiz question pagination
+// Quiz question pagination buttons
 $("#prev").on("click", function(){
+  // Move to the previous question
     if($(".question.active").index() > 0)
         $(".question.active").removeClass("active").prev().addClass("active");
+  // Remove 'calculate score' button if moving back to question 9 or lower
+    if($(".question.active").index() < 9)
+        $("#calculate-score").addClass("hidden");
+  // Dull appearance of 'prev' button when not valid
+    if($(".question.active").index() < 1)
+        $("#prev").addClass("btn-pag-invalid");
+  // Brighten appearance of 'next' button when valid
+    if($(".question.active").index() < 9)
+        $("#next").removeClass("btn-pag-invalid");
 });
 $("#next").on("click", function(){
+  // Move to the next question
     if($(".question.active").index() < $(".question").length-1)
         $(".question.active").removeClass("active").next().addClass("active");
+  // Once on question 10 show 'calculate score' button
+    if($(".question.active").index() === 9)
+        $("#calculate-score").removeClass("hidden");
+  // Brighten appearance of 'prev' button when valid
+    if($(".question.active").index() > 0)
+        $("#prev").removeClass("btn-pag-invalid");
+  // Dull appearance of 'next' button when not valid
+    if($(".question.active").index() > 8)
+        $("#next").addClass("btn-pag-invalid");
 });
 
 
